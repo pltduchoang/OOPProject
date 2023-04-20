909c44086758be6c6f6db32af36ab7ba60c89213
pltduchoang <plt.duchoang@gmail.com>
2023-04-20 16:59:58 -0600

    UtitilityCode

--- UtilityManagement/Utility/Utility101.xaml.cs
+++ UtilityManagement/Utility/Utility101.xaml.cs
@@ -1,9 +1,44 @@
+using Microsoft.Maui.Graphics.Text;
+using System.Security.Cryptography.X509Certificates;
+
 namespace UtilityManagement.Utility;
 
 public partial class Utility101 : ContentPage
 {
-	public Utility101()
+    AppartmentCreator appGlobal = null;
+    public Utility101()
+    
+    
 	{
 		InitializeComponent();
-	}
+        Database.DBConnect dBConnect = new Database.DBConnect();
+        List<AppartmentCreator> tempList = dBConnect.DataTenent();
+
+        int index = 0;
+        while (tempList[index].unitNum != 101 && index < tempList.Count())
+        {
+            index++;
+        }
+        AppartmentCreator app = tempList[index];
+        this.appGlobal = app;
+        this.TenantName.Text = ($"{app.fName} {app.lName}");
+        this.Rent.Text = app.rent.ToString();
+        this.WaterLaundry.Text = app.waterLaundry.ToString();
+        this.LastElectricity.Text= app.power.ToString();
+               
+    }
+
+    public void Calculation(object sender, EventArgs e)
+    {
+        int input = Int32.Parse(this.NewElectricity.Text);
+        int usage = input - this.appGlobal.power;
+        double cost = usage * 1.4;
+        this.ElectricityCost.Text = cost.ToString();
+        double ammountDue = appGlobal.rent + appGlobal.waterLaundry + cost;
+        this.TotalAmmount.Text = ammountDue.ToString();
+
+        string message = ($"Dear {appGlobal.fName} {appGlobal.lName} residing at Apparment {appGlobal.unitNum}! \nPlease check the below for this month rent details \nRent: {appGlobal.rent} \nFixed charge of Water and Laundry: {appGlobal.waterLaundry} \nElectricity Usage: {input} - {appGlobal.power} = {usage} ==> Cost: {cost} \nTotal Due: {ammountDue}");
+        this.SmS.Text = message;
+    }
+
 }
